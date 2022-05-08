ef45aac2ec84c3729362957c71f2d845e0d161bd
elanslacis <elanslacis@gmail.com>
2022-05-08 21:45:00 +0300

    VPC configuration till 5e

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -1,57 +1,114 @@
 provider "aws" {
-	region = var.region
-}
-
-variable "region" {
-	type  = string
-	default = "us-east-2"
+	region = "us-east-2"
 }
 
 resource "aws_vpc" "main" {
 	cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_vpc" "local" {
-	cidr_block = "10.1.0.0/16"
-}
-
-resource "aws_subnet" "public_1" {
-    vpc_id     = aws_vpc.main.id
-	cidr_block = "10.0.1.0/24"
-
     tags = {
-    Name = "public_1"
-  }
+        Name = "Main VPC"
+    }
 }
 
-resource "aws_subnet" "public_2" {
+
+resource "aws_subnet" "public" {
 	vpc_id     = aws_vpc.main.id
-	cidr_block = "10.0.2.0/24"
+	cidr_block = ["10.0.1.0/24", "10.0.2.0/24"]
 
-    tags = {
-    Name = "public_2"
-  }
-}
-
-resource "aws_subnet" "local_1" {
-	vpc_id     = aws_vpc.local.id
-	cidr_block = "10.0.3.0/24"
-
-    tags = {
-    Name = "local_1"
-  }
-}
-
-resource "aws_subnet" "local_2" {
-	vpc_id     = aws_vpc.local.id
-	cidr_block = "10.0.4.0/24"
-
-    tags = {
-    Name = "local_2"
-  }
+	tags = {
+		Name = "public"
+	}
 }
 
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+resource "aws_subnet" "private" {
+	vpc_id     = aws_vpc.main.id
+	cidr_block = ["10.0.3.0/24", "10.0.4.0/24"]
+
+		tags = {
+		Name = "private"
+	}
+}
+
+
+
+resource "aws_internet_gateway" "igw" {
+	vpc_id = aws_vpc.main.id
+
+	tags = {
+		Name = "Main Internet Gateway"
+	}
+	
+}
+
+resource "aws_eip" "nat_eip" {
+	vpc = true
+	depends_on = [aws_internet_gateway.igw]
+	tags = {
+		Name = "NAT Gateway EIP"
+	}
+}
+
+
+resource "aws_nat_gateway" "nat" {
+	allocation_id = aws_eip.nat_eip.id
+	subnet_id = aws_subnet.public.id
+
+	tags = {
+		Name = "Main NAT Gateway"
+	}
+}
+
+resource "aws_route_table" "public" {
+	vpc_id = aws_vpc.main.id
+
+	route = [ {
+	  cidr_block = "0.0.0.0/0"
+	  gateway_id = aws_internet_gateway.igw.id
+	} ]
+}
+
+resource "aws_route_table_association" "public" {
+	subnet_id = aws_subnet.public.id
+	route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+	vpc_id = aws_vpc.main.id
+
+	route = [ {
+	  cidr_block = "0.0.0.0/0"
+	  gateway_id = aws_nat_gateway.nat.id
+	} ]
+}	
+
+resource "aws_route_table_association" "private" {
+	subnet_id = aws_subnet.private.id
+	route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_security_group" "sg" {
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "HTTP"
+    cidr_blocks      = "0.0.0.0/0"
+
+  }
+    ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "HTTPS"
+    cidr_blocks      = "0.0.0.0/0"
+
+  }
+    ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "SSH"
+	cidr_blocks 	 = "62.85.31.62"
+  }
 }
